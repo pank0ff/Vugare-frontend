ed5c552472b199db337b54edab90d28c8034f237
Admin <scorpion-18krek@mail.ru>
2022-04-19 17:32:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { Link, useHistory } from "react-router-dom";
+import "./App.css";
+import Routes from "./routes/Routes";
+import useAuth from "./hooks/useAuth";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  rightToolbar: {
+    flexGrow: 1,
+  },
+  title: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  const auth = useAuth();
+  const history = useHistory();
+
+  const onLogOut = () => {
+    auth.logOut();
+    history.push("/login");
+  };
+
+  return (
+    <div className={classes.root} >
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            VUgare
+          </Typography>
+          <div className={classes.rightToolbar}>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/">
+              Posts
+            </Button>
+            <Button color="inherit" component={Link} to="/">
+              Chats
+            </Button>
+          </div>
+          {auth.isLoaded &&
+            (auth.user ? (
+              <>
+                <Button color="inherit" component={Link} to="/profile">
+                  {auth.user.firstName} {auth.user.lastName}
+                </Button>
+                <Button color="inherit" onClick={onLogOut}>
+                  Log out
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button color="inherit" component={Link} to="/login">
+                  Login
+                </Button>
+                <Button color="inherit" component={Link} to="/registration">
+                  Registration
+                </Button>
+              </>
+            ))}
+        </Toolbar>
+      </AppBar>
+
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import { Grid, makeStyles, Container, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3),
+  },
+}));
+
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm" className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h2" gutterBottom>
+            Homepage
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+          This is a social network for people who always want to be in touch :)
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default Home;
